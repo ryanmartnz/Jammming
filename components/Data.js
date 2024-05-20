7c5a3c6cb4837922d71b574d9db8813ccc09b399
ryanmartinez <martinez274@usf.edu>
2024-05-20 13:46:21 -0400

    Created initial static components

---
+++ components/Data.js
@@ -0,0 +1,17 @@
+export const trackData = [
+    {
+        songName: 'Malibu',
+        artist: 'THE DRIVER ERA',
+        album: 'Summer Mixtape',
+    },
+    {
+        songName: 'Powder',
+        artist: 'Melanie Martinez',
+        album: 'PORTALS (Deluxe)',
+    },
+    {
+        songName: "pretty isn't pretty",
+        artist: 'Olivia Rodrigo',
+        album: 'GUTS',
+    },
+];
